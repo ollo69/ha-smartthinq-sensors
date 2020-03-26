e22769e3525d7179dfa2a9e34935c4f312b3d014
ollo69 <60491700+ollo69@users.noreply.github.com>
2020-03-26 17:50:25 +0100

    Updated DeviceType class

--- custom_components/smartthinq_washer/wideq.py
+++ custom_components/smartthinq_washer/wideq.py
@@ -782,8 +782,38 @@ class Client(object):
 
 class DeviceType(enum.Enum):
     """The category of device."""
-    
+    REFRIGERATOR = 101
+    KIMCHI_REFRIGERATOR = 102
+    WATER_PURIFIER = 103
     WASHER = 201
+    DRYER = 202
+    STYLER = 203
+    DISHWASHER = 204
+    OVEN = 301
+    MICROWAVE = 302
+    COOKTOP = 303
+    HOOD = 304
+    AC = 401
+    AIR_PURIFIER = 402
+    DEHUMIDIFIER = 403
+    ROBOT_KING = 501
+    TV = 701
+    BOILER = 801
+    SPEAKER = 901
+    HOMEVU = 902
+    ARCH = 1001
+    MISSG = 3001
+    SENSOR = 3002
+    SOLAR_SENSOR = 3102
+    IOT_LIGHTING = 3003
+    IOT_MOTION_SENSOR = 3004
+    IOT_SMART_PLUG = 3005
+    IOT_DUST_SENSOR = 3006
+    EMS_AIR_STATION = 4001
+    AIR_SENSOR = 4003
+    PURICARE_AIR_DETECTOR = 4004
+    V2PHONE = 6001
+    HOMEROBOT = 9000
 
 
 class DeviceInfo(object):
