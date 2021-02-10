8dc42758a2d682b8343a629e5157ab8519f1d07a
ollo69 <60491700+ollo69@users.noreply.github.com>
2021-02-10 22:00:18 +0100

    Add version key to manifest.json

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -2,7 +2,7 @@
 Support to interface with LGE ThinQ Devices.
 """
 
-__version__ = "0.5.15"
+__version__ = "0.5.16"
 PROJECT_URL = "https://github.com/ollo69/ha-smartthinq-sensors/"
 ISSUE_URL = "{}issues".format(PROJECT_URL)
 
