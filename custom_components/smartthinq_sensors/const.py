1c6eaaefd148da68bb87d5d070e4f5441c43f605
ollo69 <oliveroandrea@libero.it>
2022-01-22 03:54:30 +0100

    Update version number

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -2,7 +2,7 @@
 Support to interface with LGE ThinQ Devices.
 """
 
-__version__ = "0.12.4"
+__version__ = "0.12.5"
 PROJECT_URL = "https://github.com/ollo69/ha-smartthinq-sensors/"
 ISSUE_URL = "{}issues".format(PROJECT_URL)
 
