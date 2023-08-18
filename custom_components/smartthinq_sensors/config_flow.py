ffac2b007992f89cc2e8f54d904432d60815f4bc
ollo69 <oliveroandrea@libero.it>
2023-08-18 22:32:43 +0000

    Use reauth flow in case of invalid credential

--- custom_components/smartthinq_sensors/config_flow.py
+++ custom_components/smartthinq_sensors/config_flow.py
@@ -1,6 +1,7 @@
 """Config flow for LG SmartThinQ."""
 from __future__ import annotations
 
+from collections.abc import Mapping
 import logging
 import re
 from typing import Any
@@ -334,6 +335,10 @@ class SmartThinQFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=schema,
             errors={CONF_BASE: base_err} if base_err else None,
         )
+
+    async def async_step_reauth(self, entry_data: Mapping[str, Any]) -> FlowResult:
+        """Perform reauth upon an API authentication error."""
+        return await self.async_step_user()
 
 
 def _dict_to_select(opt_dict: dict) -> SelectSelectorConfig:
