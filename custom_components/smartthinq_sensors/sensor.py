1dfd6e9e5942c1b3be4c6171ea1702e6521a5386
Stefan de Konink <stefan@konink.de>
2023-10-15 19:11:57 +0200

    Final fix

--- custom_components/smartthinq_sensors/sensor.py
+++ custom_components/smartthinq_sensors/sensor.py
@@ -261,6 +261,27 @@ AC_SENSORS: Tuple[ThinQSensorEntityDescription, ...] = (
         native_unit_of_measurement=PERCENTAGE,
     ),
     ThinQSensorEntityDescription(
+        key=AirConditionerFeatures.PM1,
+        name="PM1",
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.PM1,
+        native_unit_of_measurement=CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    ),
+    ThinQSensorEntityDescription(
+        key=AirConditionerFeatures.PM10,
+        name="PM10",
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.PM10,
+        native_unit_of_measurement=CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    ),
+    ThinQSensorEntityDescription(
+        key=AirConditionerFeatures.PM25,
+        name="PM2.5",
+        state_class=SensorStateClass.MEASUREMENT,
+        device_class=SensorDeviceClass.PM25,
+        native_unit_of_measurement=CONCENTRATION_MICROGRAMS_PER_CUBIC_METER,
+    ),
+    ThinQSensorEntityDescription(
         key=AirConditionerFeatures.FILTER_MAIN_LIFE,
         name="Filter Remaining Life",
         icon="mdi:air-filter",
