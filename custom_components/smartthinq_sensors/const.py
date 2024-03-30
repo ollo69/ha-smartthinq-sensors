22a33dd416eee8af9e581185504d58c2a3ee16c1
ollo69 <oliveroandrea@libero.it>
2024-03-30 22:52:57 +0000

    Update requirements and set minumum required HA version to 2024.2

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -6,8 +6,8 @@ ISSUE_URL = f"{PROJECT_URL}issues"
 
 DOMAIN = "smartthinq_sensors"
 
-MIN_HA_MAJ_VER = 2023
-MIN_HA_MIN_VER = 12
+MIN_HA_MAJ_VER = 2024
+MIN_HA_MIN_VER = 2
 __min_ha_version__ = f"{MIN_HA_MAJ_VER}.{MIN_HA_MIN_VER}.0"
 
 # general sensor attributes
