712971ce1d57c267028bf2d09435a2c76521f36b
ollo69 <oliveroandrea@libero.it>
2024-05-04 09:27:05 +0000

    Update version

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -1,6 +1,6 @@
 """Constants for LGE ThinQ custom component."""
 
-__version__ = "0.39.0"
+__version__ = "0.39.1"
 PROJECT_URL = "https://github.com/ollo69/ha-smartthinq-sensors/"
 ISSUE_URL = f"{PROJECT_URL}issues"
 
